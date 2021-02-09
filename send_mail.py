7e44bd38dacd6012f7271df889df73fc041b6324
leo-ventura30 <leonardo.ventura@outlook.com>
2021-02-08 22:32:23 -0300

    join dataframes

--- send_mail.py
+++ send_mail.py
@@ -1,23 +1,3 @@
-import smtplib
-import email.message
-import pandas as pd
-
-table_sells = pd.read_excel("./Vendas.xlsx")
-
-table_billings = table_sells[["ID Loja",
-                              "Valor Final"]].groupby("ID Loja").sum().sort_values("Valor Final", ascending=False)
-
-table_quantity = table_sells[["ID Loja",
-                              "Quantidade"]].groupby("ID Loja").sum()
-ticket_average = (table_billings["Valor Final"] /
-                  table_quantity["Quantidade"]).to_frame().rename(columns={0: "Ticket Medio"})
-
-table_complete = table_billings.join(table_quantity).join(ticket_average)
-# print(table_quantity)
-# print(ticker_average)
-print(table_complete)
-
-
 def enviar_email(nome_loja, tabela):
     server = smtplib.SMTP('smtp.gmail.com:587')
     corpo_email = f"""
