8b5dde05335ea3297da7d69ddf4b3621cb64b1e7
leo-ventura30 <leonardo.ventura@outlook.com>
2021-02-08 22:39:10 -0300

    send mail

--- send_mail.py
+++ send_mail.py
@@ -1,21 +1,19 @@
 def enviar_email(nome_loja, tabela):
     server = smtplib.SMTP('smtp.gmail.com:587')
-    corpo_email = f"""
-      <p>Prezados, </p>
-      <p>Segue relatorio de vendas</p>
-      <a href={nome_loja}>{tabela.to_html()}</a>
+    corpo_email = """
+      body mail
     """
 
     msg = email.message.Message()
-    msg['Subject'] = f"Relatorio de vendas {nome_loja}"
+    msg['Subject'] = "Assunto"
 
     # Fazer antes (apenas na 1ª vez): Ativar Aplicativos não Seguros.
     # Gerenciar Conta Google -> Segurança -> Aplicativos não Seguros -> Habilitar
     # Caso mesmo assim dê o erro: smtplib.SMTPAuthenticationError: (534,
     # Você faz o login no seu e-mail e depois entra em: https://accounts.google.com/DisplayUnlockCaptcha
-    msg['From'] = 'leohabbo1998@gmail.com'
-    msg['To'] = 'leonardofventura98@gmail.com'
-    password = "2585963741l"
+    msg['From'] = 'remetente'
+    msg['To'] = 'destinatario'
+    password = "senha do email"
     msg.add_header('Content-Type', 'text/html')
     msg.set_payload(corpo_email)
 
